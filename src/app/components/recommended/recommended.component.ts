7fb6722c0cc061e9e91626a48f1a500bbd543ea8
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2022-09-02 12:20:57 +0300

    recommended products ui

--- src/app/components/recommended/recommended.component.ts
+++ src/app/components/recommended/recommended.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-recommended',
   templateUrl: './recommended.component.html',
-  styleUrls: ['./recommended.component.css']
+  styleUrls: ['./recommended.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RecommendedComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
