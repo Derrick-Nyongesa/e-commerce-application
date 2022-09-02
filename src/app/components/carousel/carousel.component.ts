6a7545262745dbc0b95559621c2ae755a68eb51b
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2022-09-02 12:04:06 +0300

    carousel section  ui

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent implements OnInit {
+  filters = {
+    page: 1,
+    page_size: 10,
+    sort_by: 'created_at',
+    is_active: true,
+  };
 
-  constructor() { }
+  // $shoppingDeals: Observable<ShoppingDeal[]>;
+  carouselaction: boolean = false;
+  sizeCount: number;
+  index: number = 0;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
