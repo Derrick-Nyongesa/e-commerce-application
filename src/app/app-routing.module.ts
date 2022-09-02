dc2d1d01f1f1e8e13fe379fae0de1762759c7bfa
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2022-09-02 11:44:58 +0300

    home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RetailComponent } from './components/retail/retail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'retail', component:RetailComponent},
+  {
+    path: '',
+    redirectTo: '/retail',
+    pathMatch: 'full',
+  },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,81 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import {
+  TuiButtonModule,
+  TuiCalendarModule,
+  TuiHintModule,
+  TuiHostedDropdownModule,
+  TuiLoaderModule,
+  TuiTooltipModule,
+} from '@taiga-ui/core';
+import {
+  TuiBreadcrumbsModule,
+  TuiCalendarRangeModule,
+  TuiCarouselModule,
+  TuiInputCountModule,
+  TuiIslandModule,
+  TuiLineClampModule,
+  TuiPaginationModule,
+  TuiTabsModule,
+  TuiTagModule,
+  TuiTextAreaModule,
+} from '@taiga-ui/kit';
+import {
+  TuiColorPickerModule,
+  TuiInputColorModule,
+} from '@taiga-ui/addon-editor';
+import { TuiInputModule, TuiSelectModule } from '@taiga-ui/kit';
+import { TuiCardModule } from '@taiga-ui/addon-commerce';
+import { TuiTableModule } from '@taiga-ui/addon-table';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { RetailComponent } from './components/retail/retail.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { RecommendedComponent } from './components/recommended/recommended.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RetailComponent,
+    CarouselComponent,
+    RecommendedComponent,
+    ProductsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+        ReactiveFormsModule,
+        TuiCalendarModule,
+        TuiCalendarRangeModule,
+        TuiColorPickerModule,
+        TuiInputColorModule,
+        TuiSelectModule,
+        TuiInputModule,
+        TuiButtonModule,
+        TuiCarouselModule,
+        TuiTagModule,
+        TuiCardModule,
+        TuiTooltipModule,
+        TuiHintModule,
+        TuiTabsModule,
+        TuiInputCountModule,
+        TuiLineClampModule,
+        TuiButtonModule,
+        TuiPaginationModule,
+        TuiBreadcrumbsModule,
+        TuiTableModule,
+        TuiLoaderModule,
+        TuiHostedDropdownModule,
+        TuiInputModule,
+        TuiIslandModule,
+        TuiTextAreaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
